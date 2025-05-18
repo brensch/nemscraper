46aec389285faa055b16261742a6e34488233246
brensch <brendolini@protonmail.com>
2025-05-17 17:12:34 -0700

    schemas fixed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod duck;
 mod fetch;
 mod process;
+mod schema;
 
 use fetch::urls;
 use fetch::zips;
@@ -9,6 +10,7 @@ use reqwest::Client;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let client = Client::new();
+    schema::fetch_all(&client, "schemas").await?;
     let feeds = urls::fetch_current_zip_urls(&client).await?;
     let some_zip = &feeds["https://nemweb.com.au/Reports/Current/FPP/"][0];
     zips::download_zip(&client, some_zip, "zips").await?;
